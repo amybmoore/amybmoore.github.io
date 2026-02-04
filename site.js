366fefa8c1b6b404a4eb648ec1bfd4b9c0cf2f4e
Amy Moore <200131427@fvtc.edu>
2026-02-03 22:01:01 -0600

    Added welcome div

--- site.js
+++ site.js
@@ -1,1 +1,23 @@
-console.log('Amy Moore website')
+const hours = new Date().getHours() // get the current hour
+
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?
+
+
+
+if(isMorning){
+    const welcome = document.querySelector('#welcome')
+    welcome.textContent = 'Good morning. Welcome to my website.'
+}
+
+else if(isAfternoon){
+    const welcome = document.querySelector('#welcome')
+    welcome.textContent = 'Good afternoon. Welcome to my website.'
+}
+
+else{
+    const welcome = document.querySelector('#welcome')
+    welcome.textContent = 'Good evening. Welcome to my website.'
+}
+
