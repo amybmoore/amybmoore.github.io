a4b70b730b3b4965a032d914ac4fc70a5932b8b9
Amy Moore <200131427@fvtc.edu>
2026-03-14 21:49:36 -0500

    Added Pokemon images

--- site.js
+++ site.js
@@ -31,53 +31,86 @@ localStorage.setItem(key, message)
 //  secret.textContent = storedValue
 
 // Week 6 Assignment:
-const urls = [
-    // 'img/charlotte_pillow-800x476.jpg',
-    // 'img/sewing-800x476.jpg',
-    // 'img/treees-800x476.jpg',
-    // 'img/charlotte-800x476.jpg',
-    // 'img/buds-800x476.jpg'
-    'https://images.pexels.com/photos/1454360/pexels-photo-1454360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    'https://images.pexels.com/photos/933964/pexels-photo-933964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    'https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    'https://images.pexels.com/photos/1251861/pexels-photo-1251861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    'https://images.pexels.com/photos/1370296/pexels-photo-1370296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-].map(url => { (new Image()).src = url; return url })
+window.addEventListener('load', () => {
 
-const images = document.querySelectorAll('#carousel img')
+    const urls = [
+        'img/charlotte_pillow-1260x750.jpg',
+        'img/sewing-1260x750.jpg',
+        'img/trees-1260x750.jpg',
+        'img/charlotte-1260x750.jpg',
+        'img/buds-1260x750.jpg'
+        //'https://images.pexels.com/photos/1454360/pexels-photo-1454360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        //'https://images.pexels.com/photos/933964/pexels-photo-933964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    //'https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    //'https://images.pexels.com/photos/1251861/pexels-photo-1251861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        //'https://images.pexels.com/photos/1370296/pexels-photo-1370296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    ].map(url => { (new Image()).src = url; return url })
 
-let currentImage = 0
-const showImages = () => {
-    const offset = currentImage % urls.length
-    images.forEach((image, index) => {
-        const imageIndex = (index + offset + urls.length) % urls.length
-        image.src = urls[imageIndex]
+    const images = document.querySelectorAll('#carousel img')
+
+    let currentImage = 0
+    const showImages = () => {
+        const offset = currentImage % urls.length
+        images.forEach((image, index) => {
+            const imageIndex = (index + offset + urls.length) % urls.length
+            image.src = urls[imageIndex]
+        })
+        console.log('Current image srcs:', Array.from(images).map(img => img.src))
+    }
+
+    showImages()
+
+    // Images advance when page is loaded every 5 seconds
+    setInterval(() => {
+        currentImage++
+        showImages()
+        console.log('Images loaded')
+    }, 5000)
+
+    // Function to advance images on Next button click
+    const nextButton = document.querySelector('#next')
+    nextButton.addEventListener('click', () => {
+        currentImage++
+        showImages()
     })
-    console.log('Current image srcs:', Array.from(images).map(img => img.src))
+
+
+    // Function to reverse images on Previous button click
+    const prevButton = document.querySelector('#prev')
+    prevButton.addEventListener('click', () => {
+        currentImage--
+        showImages()
+    })
+
+})
+
+// Pokemon image
+//Create an arrow function called getRandomPokemon that will fetch a random pokemon from the PokeAPI. 
+// Do not have your getRandomPokemon function call the renderPokemon function. Instead, have your getRandomPokemon function return the pokemon object. 
+const getRandomPokemon = async () => {
+    const url = 'https://pokeapi.co/api/v2/pokemon/' + Math.floor(Math.random() * 150)
+    const response = await fetch(url)
+    const pokemonObj = await response.json()
+    return pokemonObj
 }
 
-showImages()
+// Create an arrow function called renderPokemon that takes a pokemon object as a parameter. This function should create an image element and append it to the DOM.
 
-// Images advance when page is loaded every 5 seconds
-setInterval(() => {
-    currentImage++
-    showImages()
-    console.log('Images loaded')
-}, 5000)
+const renderPokemon = (pokemonObj) => {
+    const parentElement = document.querySelector('#pokemon')
 
-// Function to advance images on Next button click
-const nextButton = document.querySelector('#next')
-nextButton.addEventListener('click', () => {
-    currentImage++
-    showImages()
-})
+    const img = document.createElement('img')
+    img.src = pokemonObj.sprites.front_default  // url of the image from the 'front_default' property
+    img.alt = pokemonObj.name  // name of the pokemon
+    parentElement.append(img)
+    return pokemonObj
+}
 
+//Then, call the renderPokemon function with the returned object.
+getRandomPokemon()
+    .then(pokemon => {
+        renderPokemon(pokemon)
+    })
 
-// Funciton to reverse images on Previous button click
-const prevButton = document.querySelector('#prev')
-prevButton.addEventListener('click', () => {
-    currentImage--
-    showImages()
-})
 
 
